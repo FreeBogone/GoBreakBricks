cea1d2a2f94ef813f35957e9ce0e41b2d056d6c7
FreeBogone <botruelove2002@gmail.com>
2026-08-15 15:49:19 -0500

    Boilerplate set up

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/FreeBogone/GoBreakBricks
+
+go 1.26.4
+
+require (
+	github.com/ebitengine/purego v0.10.2 // indirect
+	github.com/gen2brain/raylib-go/raylib v0.60.1 // indirect
+	github.com/jupiterrider/ffi v0.7.0 // indirect
+	golang.org/x/exp v0.0.0-20260813180055-c1d0aacb2297 // indirect
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+// declare constants
+const (
+	screenWidth  = 800
+	screenHeight = 450
+	paddleWidth  = 20
+	paddleHeight = 120
+)
+
+// declare global variables
+var (
+	running = true
+)
+
+func Init() {
+	rl.InitWindow(screenWidth, screenHeight, "GoBreakBricks")
+
+	rl.SetTargetFPS(60)
+}
+
+// func Input(g *Game) {
+//
+// }
+
+func Update() {
+}
+
+func Draw() {
+	// Draw
+	rl.BeginDrawing()
+
+	rl.ClearBackground(rl.Black)
+
+	rl.DrawText("Welcome to GoBreakBricks", 0, 0, 24, rl.White)
+
+	rl.EndDrawing()
+}
+
+func main() {
+	Init()
+
+	running = !rl.WindowShouldClose()
+
+	for running {
+
+		// Input()
+
+		Update()
+
+		Draw()
+
+		// handle window close
+		running = !rl.WindowShouldClose()
+	}
+}
